0920429cdf99326e9a545999eb09bccfa573ef12
lendvai.aron <lendvai.aron@24-DIAK16>
2026-09-24 11:07:50 +0200

    Add project files.

---
+++ la2026_09_24/Program.cs
@@ -0,0 +1,20 @@
+﻿    namespace la2026_09_24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string hw = "Hello world!";
+
+            foreach (var c in hw)
+            {
+                Console.ForegroundColor = (ConsoleColor)Random.Shared.Next(1, 16);
+                Console.WriteLine(c);
+            }
+
+            while (Console.ReadKey(true).Key !=1 ;
+
+
+        }
+    }
+}
